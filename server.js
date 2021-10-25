dc65852dc48af590c7b31874451ed43ac222eb4d
aziz <ahmed613@umn.edu>
2021-10-25 18:52:15 -0500

    changed port environment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { animals } = require('./data/animals.json');
-
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 
@@ -52,7 +52,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 }); 
 
