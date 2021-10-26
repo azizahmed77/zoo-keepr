10edf43b24328a9a9940869820a8643a2af93999
aziz <ahmed613@umn.edu>
2021-10-26 13:03:18 -0500

    lnked front-end/back-end

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-
-
 const { animals } = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -121,6 +121,21 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, () => {
